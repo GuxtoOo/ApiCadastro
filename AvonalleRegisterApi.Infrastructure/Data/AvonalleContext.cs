329d370496048679495ae2019cb46e37ac20290f
augusto santos <augustomanoel527@gmail.com>
2023-03-28 20:21:13 -0300

    🔧 Confg: Configurin all the context of api 🔧

---
+++ AvonalleRegisterApi.Infrastructure/Data/AvonalleContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AvonalleRegisterApi.Infrastructure.Data;
+public partial class AvonalleContext : DbContext
+{
+    public AvonalleContext(DbContextOptions<AvonalleContext> options)
+        : base(options) { }
+
+    public virtual DbSet<Produto> Produtos { get; set; } = null!;    
+}
--- AvonalleRegisterApi/Configurations/Configuration.cs
+++ AvonalleRegisterApi/Configurations/Configuration.cs
@@ -1,6 +1,60 @@
-﻿namespace AvonalleRegisterApi.Configurations
+﻿using AvonalleRegisterApi.Infrastructure.Data;
+using System.Text;
+
+namespace AvonalleRegisterApi.Configurations;
+
+public class Configuration
 {
-    public class Configuration
+    public static class Configuration
     {
+        public static string JwtKey { get; set; } = "ZGIxMzVlZjktYTA0OC00Mzk4LWI3YmMtNmI3ZjUyMWRmYzg0";
+
+        public static IServiceCollection AddDependencyInjectionConfig(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            
+
+            // Repository
+            
+
+            // Services
+            
+
+            return services;
+        }
+
+        public static IServiceCollection AddDbContextConfig(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("AvonalleDBConnection");
+
+            services.AddDbContext<AvonalleContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+                options.EnableSensitiveDataLogging(true);
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddJwtTokenConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            var key = Encoding.ASCII.GetBytes(JwtKey);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
     }
-}
+
