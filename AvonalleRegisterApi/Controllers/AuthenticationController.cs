9f6981803b38be875fc0fce991c3f30ea3d2ccd4
augusto santos <augustomanoel527@gmail.com>
2023-03-29 13:44:14 -0300

    ✨ Add: Authorization and JwtToken in api ✨

---
+++ AvonalleRegisterApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using AvonalleRegisterApi.Domain.Models;
+using AvonalleRegisterApi.DTOs;
+using AvonalleRegisterApi.Services;
+using AvonalleRegisterApi.Services.Interfaces;
+using AvonalleRegisterApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvonalleRegisterApi.Controllers;
+
+public class AuthenticationController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IUserService _userService;
+    private readonly TokenService _tokenService;
+
+    public AuthenticationController(IMapper mapper, IUserService userService, TokenService tokenService)
+    {
+        _mapper = mapper;
+        _userService = userService;
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("v1/login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginDto model)
+    {
+        try
+        {
+            var user = await _userService.LoginAsync(model.Logon, model.Password);
+            var mappingUser = _mapper.Map<User>(user);
+            if (mappingUser == null)
+                return BadRequest(
+                    new ResultViewModel<string>("Não foi possível realizar o login, por favor verifique as suas credenciais."));
+
+            var result = new
+            {
+                mappingUser.Logon,
+                token = _tokenService.GenerateToken(mappingUser)
+            };
+            return Ok(new ResultViewModel<dynamic>(result));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<string>("Internal server error."));
+        }
+    }
+}
---
+++ AvonalleRegisterApi/Services/TokenService.cs
@@ -0,0 +1,30 @@
+﻿using AvonalleRegisterApi.Domain.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AvonalleRegisterApi.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configurations.Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("id", user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Logon)
+            }),
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
