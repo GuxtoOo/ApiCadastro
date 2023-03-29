7dab6509c8fb71de30eb82c9722529e693c92e73
augusto santos <augustomanoel527@gmail.com>
2023-03-29 13:45:20 -0300

    ✨ Add: Post structure of User ✨

--- AvonalleRegisterApi.Infrastructure/Data/AvonalleContext.cs
+++ AvonalleRegisterApi.Infrastructure/Data/AvonalleContext.cs
@@ -7,5 +7,6 @@ public partial class AvonalleContext : DbContext
     public AvonalleContext(DbContextOptions<AvonalleContext> options)
         : base(options) { }
 
-    public virtual DbSet<Product> Produtos { get; set; } = null!;    
+    public virtual DbSet<Product> Produtos { get; set; } = null!;
+    public virtual DbSet<User> Usuarios { get; set; } = null!;
 }
---
+++ AvonalleRegisterApi/Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using AvonalleRegisterApi.Domain.Models;
+using AvonalleRegisterApi.DTOs;
+
+namespace AvonalleRegisterApi.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDto> PostAsync(UserDto model);
+        Task<UserDto> LoginAsync(string logon, string password);
+    }
+}
