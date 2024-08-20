6c9bfb5477e11abb22b556846d0ba4133677201c
augusto.santos <augusto.santos@fandi.com.br>
2024-08-20 11:21:38 -0300

    Renomeando e realocando arquivos

---
+++ RegisterApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using AvonalleRegisterApi.Domain.Models;
+using AvonalleRegisterApi.DTOs;
+using AvonalleRegisterApi.Services;
+using AvonalleRegisterApi.Services.Interfaces;
+using AvonalleRegisterApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvonalleRegisterApi.Controllers;
+
+public class AuthenticationController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IUserService _userService;
+    private readonly TokenService _tokenService;
+
+    public AuthenticationController(IMapper mapper, IUserService userService, TokenService tokenService)
+    {
+        _mapper = mapper;
+        _userService = userService;
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("v1/loginUser")]
+    public async Task<IActionResult> Login([FromBody] UserDto model)
+    {
+        try
+        {
+            var error = await _userService.PostAsync(model);
+            return (IActionResult)error;
+        }
+        catch (Exception ex)
+        {
+
+            return StatusCode(500, new ResultViewModel<string>($"Erro interno: {ex.Message}"));
+        }
+    }
+
+    [HttpGet("v1/login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginDto model)
+    {
+        try
+        {
+            var user = await _userService.LoginAsync(model.Logon, model.Password);
+            var mappingUser = _mapper.Map<User>(user);
+            if (mappingUser == null)
+                return BadRequest(
+                    new ResultViewModel<string>("Não foi possível realizar o login, por favor verifique as suas credenciais."));
+
+            var result = new
+            {
+                mappingUser.Logon,
+                token = _tokenService.GenerateToken(mappingUser)
+            };
+            return Ok(new ResultViewModel<dynamic>(result));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<string>($"Erro interno: {ex.Message}"));
+        }
+    }
+}
---
+++ RegisterApi/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using AvonalleRegisterApi.DTOs;
+using AvonalleRegisterApi.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvonalleRegisterApi.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Authorize]
+        [HttpPost("v1/user")]
+        public async Task<IActionResult> PostAsync([FromBody] UserDto model)
+        {
+            try
+            {
+                var response = await _userService.PostAsync(model);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao tentar inserir um novo usuário: {ex.Message}");
+            };
+        }
+    }
+}
